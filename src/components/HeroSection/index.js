4cb8db9efe6742247643287b909355657eaa3369
kashkama <owen.dev01@gmail.com>
2021-10-30 17:11:54 +0300

    hero/banner

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { HeroBg, HeroContainer, VideoBg } from './herosection.styled';
+import React, { useState } from 'react'
+import { HeroBg, HeroContainer, VideoBg, HeroContent, HeroHeader, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './herosection.styled';
 import Video from "./../../public/video/video.mp4";
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted type="video/mp4" src={Video}/>
             </HeroBg>
+            <HeroContent>
+                <HeroHeader>Phone care and Repairs</HeroHeader>
+                <HeroP>Professional Specialists in smartphone repair.</HeroP>
+            </HeroContent>
+            <HeroBtnWrapper>
+                <Button to="/signup" onMouseEnter={onHover} onMouseLeave={onHover}>Get started {hover ? <ArrowForward/> : <ArrowRight/>}</Button>
+            </HeroBtnWrapper>
         </HeroContainer>
     )
 }
