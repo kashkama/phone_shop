a26317644762e20c1546c4e87deedb4f3fedc1be
kashkama <owen.dev01@gmail.com>
2022-01-15 13:33:11 +0300

    add location details

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -1,18 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import {LocationMain, LocationWrapper, LocationMap, LocationInfo, BackBtn} from './location.styled'
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { GoLocation, GoMail, GoClock } from "react-icons/go";
+import { FiPhoneCall } from "react-icons/fi";
+
+import {
+  LocationMain,
+  LocationWrapper,
+  LocationMap,
+  LocationInfo,
+  BackBtn,
+  LocationHeader,
+  LocationDetailsWrapper,
+  List,
+  ItemDetails,
+  ListDetails,
+} from "./location.styled";
 
 const Location = () => {
-    const history = useHistory();
-    return (
-        <LocationMain>
-            <LocationWrapper>
-                <BackBtn onClick={() => history.goBack()}/>
-                <LocationMap/>
-                <LocationInfo/>
-            </LocationWrapper>
-        </LocationMain>
-    )
-}
+  const history = useHistory();
+  return (
+    <LocationMain>
+      <LocationWrapper>
+        <BackBtn onClick={() => history.goBack()} />
+        <LocationMap />
+        <LocationInfo>
+          <LocationHeader>Find us</LocationHeader>
+          <LocationDetailsWrapper>
+            <LocationDetails info="nairobi" icon={GoLocation} />
+            <LocationDetails info="+25400000000" icon={FiPhoneCall} />
+            <LocationDetails info="peter@gmail.com" icon={GoMail} />
+            <LocationDetails
+              info="Monday - Saturday 8am - 7pm, Saturdays 9am - 6pm, Closed Sundays"
+              icon={GoClock}
+            />
+          </LocationDetailsWrapper>
+        </LocationInfo>
+      </LocationWrapper>
+    </LocationMain>
+  );
+};
+
+const LocationDetails = (props) => {
+  let Icon = props.icon;
+  return (
+    <List>
+      <Icon style={{ color: "#fff" }} />
+      <ItemDetails>{props.info}</ItemDetails>
+    </List>
+  );
+};
 
 export default Location;
--- src/components/Location/location.styled.js
+++ src/components/Location/location.styled.js
@@ -1,45 +1,76 @@
 import styled from "styled-components";
-import { FaArrowLeft } from "react-icons/fa"
+import { FaArrowLeft } from "react-icons/fa";
 
 export const LocationMain = styled.main`
   width: 100%;
   height: 100vh;
-  `
+`;
 
 export const LocationWrapper = styled.article`
-    display: flex;
-    flex-direction: row;
-    height: 100%;
-    width: 100%;
-`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
+`;
 
 export const LocationMap = styled.div`
-    flex-grow: 2;
-    background-color: red;
-`
+  flex-grow: 2;
+  background-color: red;
+`;
 
 export const LocationInfo = styled.div`
-    flex-grow: 1;
-    background-color: #101522;
-`
+  flex-grow: 1;
+  background-color: #101522;
+`;
 
 export const BackBtn = styled(FaArrowLeft)`
-    position: absolute;
-    top: 2rem;
-    left: 0;
-    font-size: 40px;
-    text-decoration: none;
-    margin: 0 0 0 45px;
-    cursor: pointer;
-    color: #fff;
+  position: absolute;
+  top: 2rem;
+  left: 0;
+  font-size: 40px;
+  text-decoration: none;
+  margin: 0 0 0 45px;
+  cursor: pointer;
+  color: #fff;
 
-    @media screen and (max-width: 480px) {
-        margin: 10px 0 0 35px;
-        top: 0;
-    }
+  @media screen and (max-width: 480px) {
+    margin: 10px 0 0 35px;
+    top: 0;
+  }
 
-    @media screen and (max-width: 280px) {
-        margin: 10px 0 0 25px;
-        top: 0;
-    }
-`
+  @media screen and (max-width: 280px) {
+    margin: 10px 0 0 25px;
+    top: 0;
+  }
+`;
+
+export const LocationHeader = styled.header`
+  font-size: 34px;
+  padding: 20px;
+  margin-bottom: 16px;
+  color: #fff;
+  text-align: center;
+`;
+
+export const LocationDetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 50px;
+  height: 400px;
+`;
+
+export const List = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: auto;
+  width: 100%;
+  margin-bottom: 15px;
+`;
+
+
+export const ItemDetails = styled.p`
+  color: #fff;
+  font-size: 14px;
+  padding-left: 10px;
+`;
